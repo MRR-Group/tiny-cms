653ce21dd7be1977d4ccefeaec2d717fbfe14e47
Dominik Purgał <amonsir29@gmail.com>
2026-01-29 15:03:52 +0100

    Add unit tests for new handlers

---
+++ apps/api/tests/Unit/Application/Site/Handler/DeleteSiteHandlerTest.php
@@ -0,0 +1,78 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Application\Site\Handler;
+
+use App\Application\Site\Command\DeleteSiteCommand;
+use App\Application\Site\Handler\DeleteSiteHandler;
+use App\Domain\Site\Entity\Site;
+use App\Domain\Site\Repository\SiteRepositoryInterface;
+use App\Domain\Site\ValueObject\SiteId;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+
+class DeleteSiteHandlerTest extends TestCase
+{
+    private SiteRepositoryInterface&MockObject $repository;
+    private DeleteSiteHandler $handler;
+
+    protected function setUp(): void
+    {
+        $this->repository = $this->createMock(SiteRepositoryInterface::class);
+        $this->handler = new DeleteSiteHandler($this->repository);
+    }
+
+    public function testHandleDeletesSite(): void
+    {
+        $id = SiteId::generate();
+        $command = new DeleteSiteCommand($id->toString());
+
+        $site = $this->createMock(Site::class);
+        $site->method("getEditorCount")->willReturn(0);
+
+        $this->repository->expects($this->once())
+            ->method("findById")
+            ->with($this->callback(fn(SiteId $sid) => $sid->equals($id)))
+            ->willReturn($site);
+
+        $this->repository->expects($this->once())->method("delete")->with($site);
+
+        $this->handler->handle($command);
+    }
+
+    public function testHandleThrowsExceptionIfSiteNotFound(): void
+    {
+        $id = SiteId::generate();
+        $command = new DeleteSiteCommand($id->toString());
+
+        $this->repository->expects($this->once())
+            ->method("findById")
+            ->willReturn(null);
+
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage("Site not found");
+
+        $this->handler->handle($command);
+    }
+
+    public function testHandleThrowsExceptionIfSiteHasEditors(): void
+    {
+        $id = SiteId::generate();
+        $command = new DeleteSiteCommand($id->toString());
+
+        $site = $this->createMock(Site::class);
+        $site->method("getEditorCount")->willReturn(1);
+
+        $this->repository->expects($this->once())
+            ->method("findById")
+            ->willReturn($site);
+
+        $this->repository->expects($this->never())->method("delete");
+
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage("Cannot delete site with assigned editors");
+
+        $this->handler->handle($command);
+    }
+}
---
+++ apps/api/tests/Unit/Application/Site/Handler/UpdateSiteHandlerTest.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Application\Site\Handler;
+
+use App\Application\Site\Command\UpdateSiteCommand;
+use App\Application\Site\Handler\UpdateSiteHandler;
+use App\Domain\Site\Entity\Site;
+use App\Domain\Site\Repository\SiteRepositoryInterface;
+use App\Domain\Site\ValueObject\SiteId;
+use App\Domain\Site\ValueObject\SiteType;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+
+class UpdateSiteHandlerTest extends TestCase
+{
+    private SiteRepositoryInterface&MockObject $repository;
+    private UpdateSiteHandler $handler;
+
+    protected function setUp(): void
+    {
+        $this->repository = $this->createMock(SiteRepositoryInterface::class);
+        $this->handler = new UpdateSiteHandler($this->repository);
+    }
+
+    public function testHandleUpdatesSite(): void
+    {
+        $id = SiteId::generate();
+        $command = new UpdateSiteCommand(
+            $id->toString(),
+            "New Name",
+            "http://new-url.com",
+            SiteType::DYNAMIC
+        );
+
+        $site = $this->createMock(Site::class);
+        $site->expects($this->once())->method("updateName")->with("New Name");
+        $site->expects($this->once())->method("updateUrl")->with("http://new-url.com");
+        $site->expects($this->once())->method("updateType")->with(SiteType::DYNAMIC);
+
+        $this->repository->expects($this->once())
+            ->method("findById")
+            ->with($this->callback(fn(SiteId $sid) => $sid->equals($id)))
+            ->willReturn($site);
+
+        $this->repository->expects($this->once())->method("save")->with($site);
+
+        $this->handler->handle($command);
+    }
+
+    public function testHandleThrowsExceptionIfSiteNotFound(): void
+    {
+        $id = SiteId::generate();
+        $command = new UpdateSiteCommand(
+            $id->toString(),
+            "New Name",
+            "http://new-url.com",
+            SiteType::DYNAMIC
+        );
+
+        $this->repository->expects($this->once())
+            ->method("findById")
+            ->willReturn(null);
+
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage("Site not found");
+
+        $this->handler->handle($command);
+    }
+}
