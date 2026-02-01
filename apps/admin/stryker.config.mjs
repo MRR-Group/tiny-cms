757122ba39baaa9169fd9a36e63acd6371a10264
Amon <amonsir29@@gmail.com>
2026-02-01 05:55:06 +0100

    wip

--- apps/admin/stryker.config.mjs
+++ apps/admin/stryker.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('@stryker-mutator/api/core').PartialStrykerOptions} */
 const config = {
   packageManager: 'pnpm',
-  reporters: ['json', 'clear-text', 'progress'],
+  reporters: ['json', 'html', 'clear-text', 'progress'],
   testRunner: 'vitest',
   plugins: ['@stryker-mutator/vitest-runner'],
   vitest: {
